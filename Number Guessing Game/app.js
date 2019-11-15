1b7c85f78c78a3b68b0cb613b4a4917ed41fc566
dondenai <46426429+dondenai@users.noreply.github.com>
2019-11-15 12:48:44 +0200

    joc

--- Number Guessing Game/app.js
+++ Number Guessing Game/app.js
@@ -1,16 +1,45 @@
 var inputNumber = document.getElementById('number'),
   form = document.querySelector('form'),
-  maxNumber = 100,
-  generatedNumber = Math.ceil(Math.random() * maxNumber);
+  maxNumber = 10,
+  generatedNumber = 0,
+  difficulty = document.getElementById('difficulty'),
+  option = document.getElementById('options');
 
+  var difficultyMap = {
+    easy: 10,
+    medium: 100,
+    hard: 1000
+  };
   document.getElementById('legend').innerHTML =  maxNumber;
+  var selectedDifficulty=difficultyMap['easy'];
 
-function reset() {
-  location.reload();
+function newGame(difLevel) {
+  if (difLevel == 10) {
+    maxNumber = 10;
+  }
+  else if (difLevel == 100) {
+    maxNumber = 100;
+  }
+  else if (difLevel ===1000) {
+    maxNumber = 1000;
+  }
+  document.getElementById('legend').innerHTML =  maxNumber;
+  generatedNumber = Math.ceil(Math.random() * maxNumber);
+  selectedDifficulty=difLevel;
+  console.log(generatedNumber);
 }
 
+option.addEventListener('reset', function(evt){
+  newGame(selectedDifficulty);
+  evt.preventDefault();
+});
 
-console.log(generatedNumber);
+option.addEventListener('submit', function(evt){
+  var level=difficulty.options[difficulty.selectedIndex].value;
+  newGame(difficultyMap[level]);
+  evt.preventDefault();
+});
+
 form.addEventListener('submit', function(e) {
   var userNumber = inputNumber.value || 0;
   
@@ -20,20 +49,24 @@ form.addEventListener('submit', function(e) {
     alert('Numarul este prea mic :( ');
   } else {
     alert('Felicitari! Ai ghicit! :)');
-    location.reload();
+    newGame(selectedDifficulty);
   }
-
 
   e.preventDefault();
 }, false);
 
-var timeleft = 100;
-var downloadTimer = setInterval(function(){
-  document.getElementById("countdown").innerHTML = "Mai ai " + timeleft + " secunde" ;
-  timeleft -= 1;
-  if(timeleft <= 0){
-    clearInterval(downloadTimer);
-    alert('Ai pierdut! :(');
-    location.reload();
-  }
-}, 1000);
+
+  // var timeleft = 100;
+  // var downloadTimer = setInterval(function(){
+  //   document.getElementById("countdown").innerHTML = "Mai ai " + timeleft + " secunde" ;
+  //   timeleft -= 1;
+  //   if(timeleft <= 0){
+  //     clearInterval(downloadTimer);
+  //     alert('Ai pierdut! :(');
+  //     newGame(selectedDifficulty);
+  //   }
+  // }, 1000);
+
+  
+
+newGame(selectedDifficulty);
